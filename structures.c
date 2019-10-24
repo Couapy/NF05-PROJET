5ef07c9834cc73b90a15416c84e844d0b195668d
Couapy <mael.marchand@neuf.fr>
2019-10-24 08:12:06 +0200

    Sépartation des fonctions, des structures et du main

---
+++ structures.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Bagage {
+  int ticket;
+  int poids;
+  int priority;
+  int depose;
+  int embarque;
+};
+
+struct Passager {
+  char nom[64];
+  char prenom[64];
+  struct Bagage bagages[2];
+  int prioritaire;
+  char numero_billet[128];
+  char date_naissance[64];
+  char numero_siege[64];
+};
+
+struct Vol {
+  char heure_depart[64];
+  char heure_arrivee[64];
+  char destination[128];
+  struct Passager passagers[1024];
+  int places_reservees;
+  int places_libres;
+};

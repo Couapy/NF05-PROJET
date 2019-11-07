0a172ba01e91c632e8637636500a4c2fc3c1b2cc
Axel Calixte <axelcalixte@disroot.org>
2019-11-07 11:57:09 +0100

    Début de rédaction de fonction créerVol

--- sources/functionsTODO.c
+++ sources/functionsTODO.c
@@ -65,7 +65,32 @@ int peutDecoller(void) {
 }
 
 int creerVol(void){
-    // heure d'arrivée, de départ, la destination, le nombre de place totales, visa ?
+
+  Vol *vol;
+  int n; // nécessaire pour ajouter plus qu'un unique vol en utilisant une seule fois la fonction
+  
+  printf("Combien de vols voulez-vous creer ?");
+  scanf("%d", &n)
+
+  vol=(Vol*)malloc(n*sizeof(Vol));
+  
+  for (int i=0; i<n; i++){  
+    printf("Nous allons creer un vol depuis l'aeroport vers une destination.\n Ou voulez-vous vous rendre ?\n");
+    scanf("%s", vol->destination);
+    printf("Choisissez votre heure de depart :\n");
+    scanf("%s", vol->heure_depart);
+    printf("Choisissez votre heure d'arrivee :\n");
+    scanf("%s", vol->heure_arrivee);
+    printf("Choisissez le nombre total de places (libres) pour ce vol.");
+    scanf("%d", &vol->places_libres);
+    printf("Est-ce que les passagers ont besoin d'un VISA pour se rendre dans la destination prevue par le vol ?\n - 0 pour non\n - 1 pour oui");
+    scanf("%d", &vol->visa_requis);
+  }
+  
+
+
+  
+  // heure d'arrivée, de départ, la destination, le nombre de place totales, visa ?
 }
 
 int main(void) {

db83c2a80d4a66acb301c3f25cad1f57540acb09
Axel Calixte <axelcalixte@disroot.org>
2019-12-12 09:02:11 +0100

    Commenter structures.c

--- sources/structures.c
+++ sources/structures.c
@@ -1,27 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * @typedef Bagage
+ * @brief Pour un bagage de passager
+ * Comporte le numéro de ticket du bagage et si il 
+ */
 typedef struct {
   int ticket;
   int embarque;
 } Bagage;
 
+/**
+ * @typedef Siege
+ * @brief Une place de siège dans l'avion
+ * Associe un numéro de rangée et de colonne pour une place dans l'avion
+ */
 typedef struct {
   int rangee;
   int colonne;
 } Siege;
 
+/**
+ * @typedef Date
+ * @brief Structure de date
+ * Avec le jour, le mois et l'année
+ */
 typedef struct {
   int jour;
   int mois;
   int annee;
 } Date;
 
+/**
+ * @typedef Temps
+ * @brief Structure q'un "temps"
+ * Comporte un entier pour l'heure et pour les minutes
+ */
 typedef struct {
   int heure;
   int minutes;
 } Temps;
 
+/**
+ * @typedef Passager
+ * @brief Tous les éléments d'un passager
+ * Comporte le nom, prénom, la nationalité, le billet et des int bouléens pour
+ * le bon fonctionnement du programme.
+ * La structure pasager intègre aussi les structures Date, Siege et Bagage.
+ */
 typedef struct {
   char nom[64];
   char prenom[64];
@@ -39,6 +66,13 @@ typedef struct {
   int embarquer;
 } Passager;
 
+/**
+ * @typedef Vol
+ * @brief Tous les éléments d'un vol
+ * Comporte le numéro de vol, la destination, le nombre de places libres,
+ * de places réservées, de colonnes et rangées (pour la taille de l'avion).
+ * Vol intègre les structures de Date, Temps et Passager. 
+ */
 typedef struct {
   char numero_vol[8];
   char destination[128];
@@ -53,6 +87,10 @@ typedef struct {
   int sieges_colonne;
 } Vol;
 
+/**
+ * @typedef Commande
+ * @brief Permet d'associer un numéro à une fonction pour la console
+ */
 typedef struct {
   int fonction;
   char commande[24];
